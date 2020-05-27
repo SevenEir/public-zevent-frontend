dd1a7dabf2a3349fc3c5c1b65b9aeb97fece5324
JonathanWCP <jonathanwc.pinheiro@outlook.com>
2020-05-27 00:35:16 -0300

    cleaning and adding base structure

---
+++ src/components/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Hello Home</h1>
+    </div>
+  );
+}
---
+++ src/components/Menu/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+export default function Menu() {
+  return (
+    <div className="menu">
+      <h1>Hello Menu</h1>
+    </div>
+  );
+}
